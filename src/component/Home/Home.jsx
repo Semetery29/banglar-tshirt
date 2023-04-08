94e1a6bdc685188532f5c831ea717a1b57725b1e
unknown <mdazizulhakimsuny@gmail.com>
2023-04-08 14:37:45 +0600

    t-shirt not resizing

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import TShirt from '../Tshirt/TShirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
     const tshirts = useLoaderData();
     return (
-        <div>
-            <h2>This is home: {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+            {
+                tshirts.map(tshirt => <TShirt
+                    key={tshirt._id}
+                    tshirt={tshirt} />)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart/>
+            </div>
         </div>
     );
 };
